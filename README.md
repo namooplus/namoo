1c2999fc120843c467d44b132d4237155e6c2c99
namooplus <namoo.developer@gmail.com>
2024-03-16 18:09:39 +0900

    feat: initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+# namoo
+
+## Projects
+
+- namoo-note
---
+++ packages/namoo-note/src/app/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <main className={styles.main}>나무의 노트</main>;
+}
