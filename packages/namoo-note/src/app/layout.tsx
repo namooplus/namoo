1990d8b785d14e37ec41da8cf6bb64a0793e7fe4
namooplus <namoo.developer@gmail.com>
2024-03-16 18:09:39 +0900

    Initialize

---
+++ packages/namoo-note/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "나무의 노트",
+  description: "나무의 노트",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
