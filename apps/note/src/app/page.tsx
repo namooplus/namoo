c15054c0e5130e4b1f082886cbdd5a18f1d89ce3
namooplus <namoo.developer@gmail.com>
2024-04-27 16:47:19 +0900

    feat: reset css

--- apps/note/src/app/page.tsx
+++ apps/note/src/app/page.tsx
@@ -1,5 +1,9 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>나무의 노트</main>;
+  return (
+    <main className={styles.main}>
+      <h1>나무의 노트</h1>
+    </main>
+  );
 }
