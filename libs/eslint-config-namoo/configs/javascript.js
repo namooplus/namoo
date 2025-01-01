5c027eebf6d5b04af82636b9d9f2728a7b154f57
namooplus <namoo.developer@gmail.com>
2025-01-01 21:00:06 +0900

    feat: initialization

--- libs/eslint-config-namoo/configs/javascript.js
+++
@@ -1,6 +0,0 @@
-// @ts-check
-
-import jsLint from "@eslint/js";
-
-/** @type {import("@typescript-eslint/utils").TSESLint.FlatConfig.ConfigArray} */
-export default [jsLint.configs.recommended];
--- libs/eslint-config-namoo/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "eslint-config-namoo" {
-  export declare type ConfigArray =
-    import("@typescript-eslint/utils").TSESLint.FlatConfig.ConfigArray;
-
-  const configs: ConfigArray;
-  export = configs;
-}
