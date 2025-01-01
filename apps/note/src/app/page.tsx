6705fec25ea2ed9080e40de5b2131d77c25c1b40
namooplus <namoo.developer@gmail.com>
2025-01-01 21:06:51 +0900

    feat: rename

--- apps/note/src/app/page.tsx
+++ apps/note/src/app/page.tsx
@@ -1,5 +1,7 @@
-export default function Home() {
+const RootPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">note</div>
   );
-}
+};
+
+export default RootPage;
