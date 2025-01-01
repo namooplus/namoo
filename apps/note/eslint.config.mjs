f778674e51e4787d36bf643e4a8e615496c0e925
namooplus <namoo.developer@gmail.com>
2025-01-01 22:33:24 +0900

    feat: with eslint-config-namoo

--- apps/note/eslint.config.mjs
+++ apps/note/eslint.config.mjs
@@ -1,20 +1,5 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import jsLint from '@eslint/js';
-import prettierRecommendedConfig from 'eslint-plugin-prettier/recommended';
+import nextConfigs from 'eslint-config-namoo/next';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  jsLint.configs.recommended,
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  prettierRecommendedConfig,
-];
+const eslintConfig = [...nextConfigs];
 
 export default eslintConfig;
